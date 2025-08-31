cad5c034a38f5ddae8016cb35a80dcb640a51c9f
webmyn001 <bellomuhyideen0001@gmail.com>
2025-08-31 11:00:50 +0100

    New Project -Sure Send

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
+// App.js (Main Component)
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import EventSchedulePage from './Event/EventSchedulePage';
-import Footer from './Event/Footer';
-import OrganizerDashboard from './Event/OrganizerDashboard';
-import UpdateEventForm from './Event/UpdateEventForm';
-import WelcomeAndRegistration from './Event/WelcomeAndRegistration';
+import LandingPage from './SureSend/pages/LandingPage';
+import BookingPage from './SureSend/pages/BookingPage';
+import ConfirmationPage from './SureSend/pages/ConfirmationPage';
+import AdminDashboard from './SureSend/pages/AdminDashboard';
+import Header from './SureSend/components/layout/Header';
+import Footer from './SureSend/components/layout/Footer';
+import TrackingPage from './SureSend/pages/TrackingPage';
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<WelcomeAndRegistration/>} />
-        <Route path="/schedule" element={<EventSchedulePage />} />
-
-
-        {/* <Route path="/organizer" element={<OrganizerDashboard />} /> */}
-        {/* <Route path="/update/:id" element={<UpdateEventForm />} /> */}
-
-        
-
-       
-      </Routes>
-      <Footer />
+      <div className="min-h-screen bg-gradient-to-br from-blue-50 to-indigo-100">
+        <Header />
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/book" element={<BookingPage />} />
+          <Route path="/confirmation" element={<ConfirmationPage />} />
+          <Route path="/tracking" element={<TrackingPage />} />
+          <Route path="/admin/dashboard" element={<AdminDashboard />} />
+        </Routes>
+        <Footer />
+      </div>
     </Router>
   );
 }
